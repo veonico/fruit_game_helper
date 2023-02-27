a38b22983999aee20c5f35aa13be665f54694700
veonico <veonico@naver.com>
2023-02-27 17:59:53 +0900

    Digit Recognition by clustering in pregress

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import numpy as np
+import pandas as pd
 import cv2
+from sklearn.cluster import KMeans
 
 img = cv2.cvtColor(np.array(Image.open('apple.png')), cv2.COLOR_RGB2BGR)
 
@@ -16,15 +18,31 @@ contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 boxes = []
 
+# 상자 검출
 for contour in contours:
     x, y, w, h = cv2.boundingRect(contour)
-    boxes.append([x, y, w, h])
+    boxes.append([x, y, x+w, y+h])  # top-left, bottom-right
 
-for box in boxes:
-    top_left     = (box[0], box[1])
-    bottom_right = (box[0] + box[2], box[1] + box[3])
-    cv2.rectangle(img, top_left, bottom_right, (0,255,0), 2)
+imgs = []
 
-cv2.imshow('apple', img)
-cv2.waitKey(1000)
+for box_idx, box in enumerate(boxes):
+    tx1, ty1, tx2, ty2  = box
+    temp_img = cv2.resize(img[ty1:ty2, tx1:tx2, :], (10, 10), interpolation=cv2.INTER_AREA)
+
+    imgs.append(temp_img.flatten())
+
+imgs = np.concatenate([imgs])
+
+center_path = "cluster_centers.csv"
+center_data = pd.read_csv(center_path, index_col = None).values
+
+kmeans = KMeans(n_clusters = 9, init = center_data, n_init = 1, random_state = 0).fit(imgs)
+answer = kmeans.predict(imgs)
+
+print(answer)
+
+cv2.imshow("temp", imgs[1].reshape([10, 10, -1]))
+cv2.waitKey(0)
+
+
 

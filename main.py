77b9a4e4db7158afa373a19d9c68c7d1f7964aab
veonico <veonico@naver.com>
2023-03-02 16:23:06 +0900

    자동화 완성, 사과 선택 알고리즘 손 볼 필요 있음

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import mouse
 bbox_locs = utils.get_canvas_position()
 image = cv2.cvtColor(np.array(ImageGrab.grab(bbox = bbox_locs)), cv2.COLOR_BGR2RGB)
 
-# image = np.array(cv2.imread(r'./apple_images/apple_1.png'))
+bbox = np.array(bbox_locs)
+
 apple_vectors, boxes = utils.get_bboxes(image)
 digit_matrix = recognizer.recognizer(apple_vectors)
 
@@ -20,7 +21,6 @@ num_iter = 0
 padding = 10
 
 while True:
-    bbox = np.array(bbox_locs)
     num_iter += 1
     picked = utils.find_tens(digit_matrix)
     print(f"picked {num_iter} : {picked}")
@@ -34,6 +34,10 @@ while True:
             cv2.rectangle(image, start_point, end_point, (255, 255, 255), -1)
 
         # 10의 되는 경우의 수의 bounding-box 표시하기
+
+        start_point = -1
+        end_point = -1
+
         for combined_box in combined_boxes:
             start_point = np.array(combined_box[:2])
             end_point = np.array(combined_box[2:])
@@ -44,18 +48,13 @@ while True:
 
             start_point = bbox_locs[:2] + start_point - padding
             end_point = bbox_locs[:2] + end_point + padding
-            
-            mouse.move(*start_point)
-            mouse.drag(*start_point, *end_point, duration = 0.5)
 
         cv2.imshow('image', image)
         cv2.waitKey(500)
+        mouse.drag(*start_point, *end_point, duration = 0.5)
 
     else:
         print("No Combination detected")
-        break
-
-    if num_iter == 3:
         break
 
 cv2.destroyAllWindows()
